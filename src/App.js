8cf473c4e5aed4d2b94fc8f04a81a5bea164aedf
jlebatard <lebatard.2008@apside-groupe.com>
2019-11-13 17:42:14 +0100

    add login

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import Header from './components/headerComponent/header';
 import Footer from './components/footerComponent/footer';
 import HomePage from './components/pages/homePageComponent/homePage';
+import SignIn from './components/SignIn';
 
 //components
 
 function App() {
   return (
     <div className="App">
-        <Header/>
+        <SignIn/>
           <HomePage/>
         <Footer/>
     </div>
